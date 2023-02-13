e7fab11b73f1036843ae1dc273f8daac9eb24175
Abhishek Kumar Patel <115661682+Abhishekkumarpate@users.noreply.github.com>
2023-02-13 21:42:14 +0530

    Rename 1.py to Basic1.py

--- 1.py
+++
@@ -1,5 +0,0 @@
-print("Abhishek Patel")
-
-print("Hey i am Abhishek Patel")
-
-print("Hey i am a good boy\n and this viewer is also a good boy\ girl")
---
+++ Basic1.py
@@ -0,0 +1,5 @@
+print("Abhishek Patel")
+
+print("Hey i am Abhishek Patel")
+
+print("Hey i am a good boy\n and this viewer is also a good boy\ girl")
